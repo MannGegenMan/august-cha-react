9a387c0ceace7d4ffa8131c8fa2cc63844d5859f
MannGegenMan <117568231+MannGegenMan@users.noreply.github.com>
2024-05-04 04:30:20 +0500

    -

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Button from '../Buttons/Button';
 import {useTelegram} from "../../hooks/useTelegram";
 
 const Header = () => {
-    const {user, onClose} = useTelegram();
+    const {user, tg, onClose} = useTelegram();
 
     return (
         <div className={'header'}>
